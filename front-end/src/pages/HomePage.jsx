10c0a4147c228f5b9e0a45435864daa483a5938a
Angel Gonzalez <angelgonzalez2118@gmail.com>
2021-08-07 15:40:43 -0400

    add home page jumbotron overlay with text and button. Changed nav z-index

--- front-end/src/pages/HomePage.jsx
+++ front-end/src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../components/Header";
 import Nav from "../components/Nav";
 import Footer from "../components/Footer";
+import Button from "../components/Button";
 
 // Nav Logo
 import LogoWithBackground from "../images/logo_with_gray_background.svg";
@@ -29,9 +30,17 @@ export default function HomePage() {
         pageTitle=""
         />
 
-      <div className="home-page-jumbotron">
-        <video muted="muted" loop="loop" src="https://luis-pagan.s3.us-east-2.amazonaws.com/Nation_Gaurd_Video.mp4"></video>
-      </div>
+      <section className="home-page-jumbotron">
+        <div className="jumbotron-overlay">
+          <div>
+            <h1>Find the Perfect MOS for you.</h1>
+            <Button btnClass="sm-btn yellow-btn" btnText="WATCH NOW" />
+          </div>
+        </div>
+        <video autoplay="autoplay" muted="muted" loop="loop" src="https://luis-pagan.s3.us-east-2.amazonaws.com/Nation_Gaurd_Video.mp4"></video>
+      </section>
+
+      <section className="benefits"></section>
 
       <Footer
         footerClass="yellow-footer"
